943bfbdbc592ed5d045a9da193d96cd91ef394a9
Virgil Roger <roger.virgil@gmail.com>
2016-05-30 09:48:44 +0200

    Added progress bar

--- src/classes/SimulatedAnnealing.py
+++ src/classes/SimulatedAnnealing.py
@@ -6,6 +6,7 @@
 import random
 import math
 import copy
+import sys
 
 class SimulatedAnnealing:
 
@@ -14,6 +15,17 @@ class SimulatedAnnealing:
 
     def getLog(self):
         return self.log
+
+    def progress(self, count, total, status=''):
+        bar_len = 60
+        filled_len = int(round(bar_len * count / float(total)))
+
+        percents = round(100.0 * count / float(total), 1)
+        bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+        sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+        sys.stdout.flush()  # As suggested by Rom Ruben (see: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console/27871113#comment50529068_27871113)
+
 
     # _initialTemp : 
     # _nbTempChanges : 
@@ -47,11 +59,11 @@ class SimulatedAnnealing:
                         current = copy.deepcopy(new)
                         currentValue = newValue
 
+                self.progress( j + i * _nbSteps, _nbSteps * _nbTempChanges)
+
             # endfor j
 
             temperature = self.tempDecayal(temperature, _mu)
-
-            print current.getValue()
 
         # endfor i
 
